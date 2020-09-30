8a8347435f6944e05d9db46428a2f06c1f23c88c
Vladimir <70747356+wovka27@users.noreply.github.com>
2020-09-30 15:54:43 +0700

    test

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function fn() {
+   let el = document.getElementsByTagName('p');
+   for (el of el) {
+      el.innerHTML = 'Иди на хуй!';
+      el.style.color = 'purple';
+      el.style.margin = '0 auto';
+      el.style.fontSize = '70px';
+      el.style.fontWeight = '900';
+      el.style.fontFamily = 'Arial';
+      el.style.transition = '1.3s';
+      el.style.transform = 'rotate(2880deg)';
+      el.style.textAlign = 'center';
+   }
+};
+
+function fn2() {
+   let t = document.getElementsByTagName('p');
+   for (t of t) {
+      t.innerHTML = '';
+      t.style.color = '';
+      t.style.margin = '';
+      t.style.fontSize = '';
+      t.style.fontWeight = '';
+      t.style.fontFamily = '';
+      t.style.transition = '';
+      t.style.transform = '';
+      t.style.textAlign = '';
+   }
+};
